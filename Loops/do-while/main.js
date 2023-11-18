ce36e29db11ce5ff3da93f61bee85f27b44a0778
Bahodir <choriyevbahodir0>
2023-11-18 12:48:41 +0500

    nothing changed

--- Loops/do-while/main.js
+++ Loops/do-while/main.js
@@ -82,37 +82,9 @@
 
 
 
-// LOOPS HOMEWORK
-// let a = "#"
-// while (a <= "##########") {
-//     console.log(a)
-//     a = a + "#"
-// }
-
-
-
-
-// let a = prompt("Ism kiriting") || "Shohjahon"
-// let b = " "
-// let c = a.length - 1;
-// while ( c >= 0) {
-//     b = b + a[c]
-//     c--
-// }
-// console.log(b)
-
-
-
-
-// FOR HOMEWORK
-let a = prompt("Ism kiriting") || "Shohjahon"
-let b = " "
-
-for (let c = a.length - 1; c >= 0; c--) {
-    b = b + a[c]
+//LOOPS HOMEWORK
+let a = "#"
+while (a <= "##########") {
+    console.log(a)
+    a = a + "#"
 }
-console.log(b)
-
-
-
-
